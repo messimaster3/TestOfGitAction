a232e27be706fc9ea3af8d5217b7aa4248e154a1
Thomas Winther <47111266+messimaster3@users.noreply.github.com>
2021-11-17 15:01:29 +0100

    added error in file for test

--- test.py
+++ test.py
@@ -11,7 +11,12 @@ class TestSum(unittest.TestCase):
         data = [1, 2, 3]
         result = sum(data)
         self.assertEqual(result, 6)
-
-
+        
+        
+        
+        
+        
+        
+        
 if __name__ == '__main__':
     unittest.main()
